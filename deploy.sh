ea2c17ecfe56e2aa6ee5551388b19b76d39fce69
Andersson Quintero <anderssonq@gmail.com>
2021-04-05 09:17:58 -0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:anderssonq/intelimetrica.git master:gh-pages
+git push -f git@github.com:anderssonq/intelimetrica.git master:gh-pages
 
 cd -
